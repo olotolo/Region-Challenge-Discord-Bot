ae69096d42a5502f559dcd52d08c22287c6e09c5
olotolo <olotolo@web.de>
2024-08-12 13:30:43 +0200

    Setup

---
+++ index.js
@@ -0,0 +1,72 @@
+//nodemon
+const {Client, IntentsBitField, MessageActivityType} = require('discord.js');
+require ('dotenv').config();
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+const regions = [
+    "Bandle City",
+    "Bilgewater",
+    "Demacia",
+    "Ionia",
+    "Ixtal",
+    "Noxus",
+    "Piltover",
+    "Shadow Isles",
+    "Shurima",
+    "Targon",
+    "The Freljord",
+    "The Void",
+    "Zaun"
+];
+
+const images = [
+    "BandleCity.png",
+    "Bilgewater.png",
+    "Demacia.png",
+    "Ionia.png",
+    "Ixtal.png",
+    "Noxus.png",
+    "Piltover.png",
+    "ShadowIsles.png",
+    "Shurima.png",
+    "Targon.png",
+    "Freljord.png",
+    "Void.png",
+    "Zaun.png"
+];
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online`);
+});
+
+client.on('messageCreate', (message) => {
+    if(message.author.bot) {
+        return;
+    }
+    if(message.content.toLowerCase() == 'regions'.toLowerCase()) {
+        let text = "";
+        for (var i = 0; i < regions.length; i++) {
+            text += regions[i] + "\n";
+        }
+        message.channel.send(text);
+    }
+    for(var i = 0; i < regions.length; i++) {
+        if(message.content.toLowerCase() == regions[i].toLowerCase()) {
+            message.channel.send({
+                
+                content: regions[i], files: [`./images/${images[i]}`]
+            });}
+        }
+    }
+
+);
+
+
+
+client.login(process.env.TOKEN);
